3de54c1b28c3619956eaa35bf4d6290a587c025d
Sujithra S <padmapriya13451@gmail.com>
2025-04-09 17:06:01 +0530

    Update Propertyutil.java

--- Java coding/InsuranceSystem/src/util/Propertyutil.java
+++ Java coding/InsuranceSystem/src/util/Propertyutil.java
@@ -5,27 +5,35 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Propertyutil {
-    public static Properties loadProperties() {
+
+    // Method to load properties file and return the connection string
+    public static String getConnectionString(String fileName) {
         Properties properties = new Properties();
-        try (InputStream input = Propertyutil.class.getClassLoader().getResourceAsStream("dbconfig.properties")) {
+
+        // Load the properties file from resources
+        try (InputStream input = Propertyutil.class.getClassLoader().getResourceAsStream(fileName)) {
             if (input == null) {
-                System.err.println("Unable to find dbconfig.properties in resources.");
+                System.err.println("Unable to find " + fileName);
                 return null;
             }
             properties.load(input);
         } catch (IOException e) {
-            System.err.println("Failed to load database properties.");
+            System.err.println("Failed to load properties file " + fileName);
             e.printStackTrace();
+            return null;
         }
-        return properties;
-    }
 
-    public static String getPropertyString() {
-        Properties properties = loadProperties();
-        if (properties == null) return null;
-        
-        return properties.getProperty("db.url") + "?user=" + properties.getProperty("db.username") +
-                "&password=" + properties.getProperty("db.password");
+        // Extract database properties
+        String url = properties.getProperty("db.url");
+        String user = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
+
+        if (url == null || user == null || password == null) {
+            System.err.println("Missing required database properties in " + fileName);
+            return null;
+        }
+
+        // Return the formatted connection string
+        return url + "?user=" + user + "&password=" + password;
     }
 }
-
